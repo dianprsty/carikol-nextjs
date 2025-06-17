f03b1f48546874a45031bad47f036a4bd01f90f2
Dhafina <Dhafinanurazizah7@gmail.com>
2025-06-17 18:00:10 +0700

    add brand

---
+++ src/app/brand/page.tsx
@@ -0,0 +1,12 @@
+import DashboardBrand from "@/components/brand/DashboardBrand";
+
+export default function DashboardBrandSection() {
+  return (
+    <div>
+      <main> 
+        <DashboardBrand />
+      </main>
+    </div>
+  );
+}
+
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -105,7 +105,7 @@ const AppNavbar = ({
         {
           title: "Cari KOL",
 
-          url: "#",
+          url: "/brand",
         },
       ],
     },
