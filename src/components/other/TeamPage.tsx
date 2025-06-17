1f0e7ea7bc4c2545948658abc872513b595987a6
Muhammad Dzulfiqar <muhammaddzulfiqar03@gmail.com>
2025-06-17 13:24:29 +0700

    Final Revisi Team page

--- src/components/other/TeamPage.tsx
+++ src/components/other/TeamPage.tsx
@@ -101,13 +101,13 @@ const Profile = [
     },
 
     {
-        nama: "Dhafina",
-        motivasi: "stecu stecu",
-        image: "",
+        nama: "Dhafina Nur Azizah",
+        motivasi: "Mahasiswa Program Studi Teknik Informatika di Institut Asia Malang. Memiliki ketertarikan yang kuat dalam bidang kepenulisan dan telah mengembangkan keterampilan dalam menulis bebas serta artikel. Selain aktif di bidang akademik, Saya juga tengah mendalami keterampilan tambahan seperti bahasa Jepang dan analisis data.",
+        image: "https://drive.google.com/uc?export=view&id=1eQbhhiCv_z_4bt2ywbAiYo2dJJ1BYUIu",
         buttonElement: [
             {
                 title: 'Github',
-                url: "",
+                url: "https://github.com/Dhafina",
                 Color: {
                     from: '#333333',
                     to: '#000000',
@@ -116,7 +116,7 @@ const Profile = [
             },
             {
                 title: 'Linkedin',
-                url: "",
+                url: "https://id.linkedin.com/in/dhafina-nur-azizah-6a7792212",
                 Color: {
                     from: '#0A66C2',
                     to: '#004182',
@@ -125,7 +125,7 @@ const Profile = [
             },
             {
                 title: 'Instagram',
-                url: "",
+                url: "https://www.instagram.com/finafinn1?igsh=MWRwOGs0emRoZnVhcQ==",
                 Color: {
                     from: '#f09433',
                     via: '#e6683c', // opsional jika ingin 3 warna gradasi
@@ -169,78 +169,78 @@ export default function TeamSection() {
 
 
 
-                     {Profile.map((item, index) => {
-    const { nama, motivasi, image, buttonElement } = item;
+            {Profile.map((item, index) => {
+                const { nama, motivasi, image, buttonElement } = item;
 
 
-    if (nama === "Dian Prasetyo") {
-        return  <div key={index} className="mx-auto mb-3">
-            <div className="px-6 mt-10 mr-0 sm:mr-24 sm:ml-24 sm:px-16 lg:mt-0 lg:px-0 lg:pt-0 lg:flex lg:items-center lg:gap-x-10">
-                {/* Konten teks di kiri */}
-                <div className="py-5 pr-0 mx-auto text-center sm:pr-5 lg:mx-0 lg:flex-auto lg:py-20 lg:text-left lg:w-1/2">
-                    <AnimatedOnScroll delay={0.4}>
-                        <div>
-                            <h2 className="font-semibold text-teal-600 text-base/7">
-                                As Kak {nama.split(" ")[0]}
-                            </h2>
-                            <p className="mt-2 text-2xl font-semibold tracking-tight text-gray-900 text-pretty lg:text-4xl md:text-base">
-                                {nama}
-                            </p>
-                            <p className="mt-6 text-sm text-gray-600 lg:text-lg md:text-base text-pretty">
-                                {motivasi}
-                            </p>
+                if (nama === "Dian Prasetyo") {
+                    return <div key={index} className="mx-auto mb-3">
+                        <div className="px-6 mt-10 mr-0 sm:mr-24 sm:ml-24 sm:px-16 lg:mt-0 lg:px-0 lg:pt-0 lg:flex lg:items-center lg:gap-x-10">
+                            {/* Konten teks di kiri */}
+                            <div className="py-5 pr-0 mx-auto text-center sm:pr-5 lg:mx-0 lg:flex-auto lg:py-20 lg:text-left lg:w-1/2">
+                                <AnimatedOnScroll delay={0.4}>
+                                    <div>
+                                        <h2 className="font-semibold text-teal-600 text-base/7">
+                                            As Kak {nama.split(" ")[0]}
+                                        </h2>
+                                        <p className="mt-2 text-2xl font-semibold tracking-tight text-gray-900 text-pretty lg:text-4xl md:text-base">
+                                            {nama}
+                                        </p>
+                                        <p className="mt-6 text-sm text-gray-600 lg:text-lg md:text-base text-pretty">
+                                            {motivasi}
+                                        </p>
+                                    </div>
+
+                                    <div className="flex gap-x-6 justify-center items-center py-5 lg:justify-start">
+                                        {buttonElement.map((subItem, subindex) => {
+                                            const { title, url, Color } = subItem;
+                                            const gradient = Color.via
+                                                ? `linear-gradient(to right, ${Color.from}, ${Color.via}, ${Color.to})`
+                                                : `linear-gradient(to right, ${Color.from}, ${Color.to})`;
+
+                                            return (
+                                                <Button
+                                                    key={subindex}
+                                                    variant="default"
+                                                    size="default"
+                                                    asChild
+                                                    style={{
+                                                        background: gradient,
+                                                        color: Color.text,
+                                                        border: 'none',
+                                                    }}
+                                                >
+                                                    <a href={url} target="_blank" rel="noopener noreferrer">
+                                                        {title}
+                                                    </a>
+                                                </Button>
+                                            );
+                                        })}
+                                    </div>
+                                </AnimatedOnScroll>
+                            </div>
+
+                            {/* Gambar di kanan */}
+                            <AnimatedOnScroll delay={0.2}>
+                                <div className="overflow-hidden relative z-10 flex justify-center items-center">
+                                    <Image
+                                        key={index}
+                                        className="rounded-full border-2 border-teal-500 ring-4 bg-teal-600/5 ring-white/10 w-[250px] h-auto lg:w-[350px]"
+                                        src={image}
+                                        alt="App screenshot"
+                                        width={400}
+                                        height={400}
+                                    />
+                                </div>
+                            </AnimatedOnScroll>
                         </div>
-
-                        <div className="flex gap-x-6 justify-center items-center py-5 lg:justify-start">
-                            {buttonElement.map((subItem, subindex) => {
-                                const { title, url, Color } = subItem;
-                                const gradient = Color.via
-                                    ? `linear-gradient(to right, ${Color.from}, ${Color.via}, ${Color.to})`
-                                    : `linear-gradient(to right, ${Color.from}, ${Color.to})`;
-
-                                return (
-                                    <Button
-                                        key={subindex}
-                                        variant="default"
-                                        size="default"
-                                        asChild
-                                        style={{
-                                            background: gradient,
-                                            color: Color.text,
-                                            border: 'none',
-                                        }}
-                                    >
-                                        <a href={url} target="_blank" rel="noopener noreferrer">
-                                            {title}
-                                        </a>
-                                    </Button>
-                                );
-                            })}
-                        </div>
-                    </AnimatedOnScroll>
-                </div>
-
-                {/* Gambar di kanan */}
-                <AnimatedOnScroll delay={0.2}>
-                    <div className="overflow-hidden relative z-10 flex justify-center items-center">
-                        <Image
-                            key={index}
-                            className="rounded-full border-2 border-teal-500 ring-4 bg-teal-600/5 ring-white/10 w-[250px] h-auto lg:w-[350px]"
-                            src={image}
-                            alt="App screenshot"
-                            width={400}
-                            height={400}
-                        />
                     </div>
-                </AnimatedOnScroll>
-            </div>
-        </div>
-    }
+                }
 
-    return null;
-})}
+                return null;
+            })}
 
-<hr />
+            <hr />
 
 
             {Profile.map((item, index) => {
@@ -311,81 +311,81 @@ export default function TeamSection() {
                     </div>
                 }
                 return null
-                
+
             })}
 
             <hr />
 
-                                 {Profile.map((item, index) => {
-    const { nama, motivasi, image, buttonElement } = item;
+            {Profile.map((item, index) => {
+                const { nama, motivasi, image, buttonElement } = item;
 
 
-    if (nama === "Dhafina") {
-        return  <div key={index} className="mt-20 mx-auto mb-3">
-            <div className="px-6 mt-10 mr-0 sm:mr-24 sm:ml-24 sm:px-16 lg:mt-0 lg:px-0 lg:pt-0 lg:flex lg:items-center lg:gap-x-10">
-                {/* Konten teks di kiri */}
-                <div className="py-5 pr-0 mx-auto text-center sm:pr-5 lg:mx-0 lg:flex-auto lg:py-20 lg:text-left lg:w-1/2">
-                    <AnimatedOnScroll delay={0.4}>
-                        <div>
-                            <h2 className="font-semibold text-teal-600 text-base/7">
-                                As Kak {nama.split(" ")[0]}
-                            </h2>
-                            <p className="mt-2 text-2xl font-semibold tracking-tight text-gray-900 text-pretty lg:text-4xl md:text-base">
-                                {nama}
-                            </p>
-                            <p className="mt-6 text-sm text-gray-600 lg:text-lg md:text-base text-pretty">
-                                {motivasi}
-                            </p>
+                if (nama === "Dhafina Nur Azizah") {
+                    return <div key={index} className="mt-20 mx-auto mb-3">
+                        <div className="px-6 mt-10 mr-0 sm:mr-24 sm:ml-24 sm:px-16 lg:mt-0 lg:px-0 lg:pt-0 lg:flex lg:items-center lg:gap-x-10">
+                            {/* Konten teks di kiri */}
+                            <div className="py-5 pr-0 mx-auto text-center sm:pr-5 lg:mx-0 lg:flex-auto lg:py-20 lg:text-left lg:w-1/2">
+                                <AnimatedOnScroll delay={0.4}>
+                                    <div>
+                                        <h2 className="font-semibold text-teal-600 text-base/7">
+                                            As Kak {nama.split(" ")[0]}
+                                        </h2>
+                                        <p className="mt-2 text-2xl font-semibold tracking-tight text-gray-900 text-pretty lg:text-4xl md:text-base">
+                                            {nama}
+                                        </p>
+                                        <p className="mt-6 text-sm text-gray-600 lg:text-lg md:text-base text-pretty">
+                                            {motivasi}
+                                        </p>
+                                    </div>
+
+                                    <div className="flex gap-x-6 justify-center items-center py-5 lg:justify-start">
+                                        {buttonElement.map((subItem, subindex) => {
+                                            const { title, url, Color } = subItem;
+                                            const gradient = Color.via
+                                                ? `linear-gradient(to right, ${Color.from}, ${Color.via}, ${Color.to})`
+                                                : `linear-gradient(to right, ${Color.from}, ${Color.to})`;
+
+                                            return (
+                                                <Button
+                                                    key={subindex}
+                                                    variant="default"
+                                                    size="default"
+                                                    asChild
+                                                    style={{
+                                                        background: gradient,
+                                                        color: Color.text,
+                                                        border: 'none',
+                                                    }}
+                                                >
+                                                    <a href={url} target="_blank" rel="noopener noreferrer">
+                                                        {title}
+                                                    </a>
+                                                </Button>
+                                            );
+                                        })}
+                                    </div>
+                                </AnimatedOnScroll>
+                            </div>
+
+                            {/* Gambar di kanan */}
+                            <AnimatedOnScroll delay={0.2}>
+                                <div className="overflow-hidden relative z-10 flex justify-center items-center">
+                                    <Image
+                                        key={index}
+                                        className="rounded-full border-2 border-teal-500 ring-4 bg-teal-600/5 ring-white/10 w-[250px] h-auto lg:w-[350px]"
+                                        src={image}
+                                        alt="App screenshot"
+                                        width={400}
+                                        height={400}
+                                    />
+                                </div>
+                            </AnimatedOnScroll>
                         </div>
-
-                        <div className="flex gap-x-6 justify-center items-center py-5 lg:justify-start">
-                            {buttonElement.map((subItem, subindex) => {
-                                const { title, url, Color } = subItem;
-                                const gradient = Color.via
-                                    ? `linear-gradient(to right, ${Color.from}, ${Color.via}, ${Color.to})`
-                                    : `linear-gradient(to right, ${Color.from}, ${Color.to})`;
-
-                                return (
-                                    <Button
-                                        key={subindex}
-                                        variant="default"
-                                        size="default"
-                                        asChild
-                                        style={{
-                                            background: gradient,
-                                            color: Color.text,
-                                            border: 'none',
-                                        }}
-                                    >
-                                        <a href={url} target="_blank" rel="noopener noreferrer">
-                                            {title}
-                                        </a>
-                                    </Button>
-                                );
-                            })}
-                        </div>
-                    </AnimatedOnScroll>
-                </div>
-
-                {/* Gambar di kanan */}
-                <AnimatedOnScroll delay={0.2}>
-                    <div className="overflow-hidden relative z-10 flex justify-center items-center">
-                        <Image
-                            key={index}
-                            className="rounded-full border-2 border-teal-500 ring-4 bg-teal-600/5 ring-white/10 w-[250px] h-auto lg:w-[350px]"
-                            src={image}
-                            alt="App screenshot"
-                            width={400}
-                            height={400}
-                        />
                     </div>
-                </AnimatedOnScroll>
-            </div>
-        </div>
-    }
+                }
 
-    return null;
-})}
+                return null;
+            })}
         </section>
     )
 
